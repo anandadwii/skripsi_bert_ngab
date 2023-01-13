73baf61d4df9a27d8c174e45c9431dafb226c459
anandadwii <namikaze.anandadwi@gmail.com>
2023-01-14 02:04:26 +0700

    feat: add function to remove emoji and try exception

--- engine_crawler.py
+++ engine_crawler.py
@@ -1,6 +1,19 @@
 import snscrape.modules.twitter as crawler
 import re
 import pandas as pd
+import traceback
+import emoji
+
+
+def get_emoji_regexp():
+    # Sort emoji by length to make sure multi-character emojis are
+    # matched first
+    emojis = sorted(emoji.EMOJI_DATA, key=len, reverse=True)
+    pattern = u'(' + u'|'.join(re.escape(u) for u in emojis) + u')'
+    return re.compile(pattern)
+
+
+begone_emoji = get_emoji_regexp()
 
 
 def remove_backslash_n(value):
@@ -10,26 +23,29 @@ def remove_backslash_n(value):
 query = '("kinerja polri" OR "kinerja polisi")'
 time_start = '2022-10-01'
 time_end = '2022-12-31'
-file_name = r'crawling twitter kinerja polisi or kinerja polri.xlsx'
+file_name = r'mengetes.xlsx'
 tweet_list = []
 sheet_name = "data oktober 2022"
 
+try:
+    for i, tweet in enumerate(
+            crawler.TwitterSearchScraper(f'{query} until:{time_end} since:{time_start}').get_items()):
+        try:
+            if i > 10000:
+                break
+            result = tweet.content.lower()
+            result = re.sub(r'(@|https?)\S+|#', '', result).replace("&amp;", "dan")
+            result = remove_backslash_n(result).strip()
+            result = begone_emoji.sub(repl='', string=result)
+            tweet_list.append(result)
+        except:
+            pass
 
-for i, tweet in enumerate(
-        crawler.TwitterSearchScraper(f'{query} until:{time_end} since:{time_start}').get_items()):
-    if i > 10000:
-        break
-    result = tweet.content.lower()
-    result = re.sub(r'(@|https?)\S+|#', '', result)
-    result = result.replace("&amp;", "dan")
-    result = remove_backslash_n(result)
-    result = result.strip()
-    tweet_list.append(result)
-
-
-print(f"{len(tweet_list)} tweets scraped!")
-print("removing duplicate tweets")
-result_ = list(set(tweet_list))
-print(f"{len(tweet_list)} tweets left after cleaning")
-data_frame = pd.DataFrame(result_)
-data_frame.to_excel(file_name, sheet_name=sheet_name, index=False)
+    print(f"{len(tweet_list)} tweets scraped!")
+    print("removing duplicate tweets")
+    result_ = list(set(tweet_list))
+    print(f"{len(result_)} tweets left after cleaning")
+    data_frame = pd.DataFrame(result_)
+    data_frame.to_excel(file_name, sheet_name=sheet_name, index=False)
+except:
+    traceback.print_exc()
