68a8b759aeb131d3072a4d505933953ee7ab2f65
anandadwii <namikaze.anandadwi@gmail.com>
2023-01-13 22:29:38 +0700

    feat: add cleaning data while crawling at twitter

--- engine_crawler.py
+++ engine_crawler.py
@@ -1,4 +1,11 @@
 import snscrape.modules.twitter as crawler
+import re
+
+
+def remove_n(value):
+    return ''.join(value.splitlines())
+
+
 query = 'kinerja polri'
 time_start = '2022-01-06'
 time_end = '2022-01-07'
@@ -6,5 +13,13 @@ tweet_list = []
 
 for i, tweet in enumerate(
         crawler.TwitterSearchScraper(f'{query} until:{time_end} since:{time_start}').get_items()):
-    print(tweet)
+    if i > 100:
+        break
+    result = tweet.content.lower()
+    result = re.sub(r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)','',result)
+    # result = remove_n(result)
+    result = result.strip()
+    tweet_list.append(result)
+    set(tweet_list)
 
+print(tweet_list)
